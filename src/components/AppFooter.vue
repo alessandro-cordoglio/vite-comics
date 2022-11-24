62673954ca224014a16dbbfdb624a01cea2c0e8e
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-11-24 21:19:35 +0100

    add: global scss, struttura header

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,6 +12,6 @@ export default {
     <div>footer</div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,50 @@
+<!-- SCRIPT -->
 <script>
-
-
 export default {
-  components: {
+    data(){
+       return{
+            links:[
+                "CHARACTERS",
+                "COMICS",
+                "MOVIES",
+                "TV",
+                "GAMES",
+                "COLLECTIBLES",
+                "VIDEOS",
+                "FANS",
+                "NEWS",
+                "SHOP"
+            ]     
+       }
+    },
+    components: {
    
   }
 }
 </script>
 
+<!-- TEMPLATE -->
 <template>
-    <div>header</div>
+    <div class="container_ header_container">
+        <div class="logo">
+            <img src="../assets/dc-logo.png" alt="">
+        </div>
+        <nav class="nav_layout">
+            <ul>
+                <li v-for="link in links"><a href="#">{{link}}</a></li>
+            </ul>
+        </nav>
+    </div>
 </template>
 
-<style lang="scss">
-
+<!-- STYLE -->
+<style lang="scss" scoped>
+    .header_container{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .nav_layout{
+        height: 100%;
+    }
 </style>
