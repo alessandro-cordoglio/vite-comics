9090b15f8141274e97193ac4a7f56a83bd3a877a
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-11-25 02:44:02 +0100

    add: stile sezione 1 footer, background sezione 2

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,6 +2,33 @@
 
 
 export default {
+  data(){
+    return{
+      items:[
+              {
+                image: 'buy-comics-digital-comics.png',
+                info:'DIGITAL COMICS'
+              } ,
+              {
+                image: 'buy-comics-merchandise.png',
+                info:'DC MERCHANDISE'
+              } ,
+              {
+                image: 'buy-comics-subscriptions.png',
+                info:'SUBSCRIPTION'
+              } ,
+              {
+                image: 'buy-comics-shop-locator.png',
+                info:'COMIC SHOP LOCATOR'
+              } ,
+              {
+                image: 'buy-dc-power-visa.svg',
+                info:'DC POWER VISA'
+              } ,
+            ]
+    }
+
+  },
   components: {
    
   }
@@ -9,9 +36,39 @@ export default {
 </script>
 
 <template>
-    <div>footer</div>
+  <section class="item_section">
+    <div class="container_">
+      <div class="items">
+        <div class="item"  v-for="item in items">
+          <div class="item_image">
+            <img :src="'/src/assets/'+ item.image" alt="">
+          </div>
+          <div class="item_info">{{item.info}}</div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="small_info--section">
+    
+  </section>
 </template>
 
 <style lang="scss" scoped>
+  .items, .item{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .item{
+      padding: 40px 20px;
+      color: white;
 
+      .item_info{
+        font-size: 14px;
+        margin-left: 10px;
+      }
+      img{
+        width: 40px;
+      }
+    }
+  }
 </style>
